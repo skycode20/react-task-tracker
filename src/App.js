29046ed73b3dd249b534718c36ad9d19c4e37223
Skyler R <skyler.rencher@gmail.com>
2021-02-27 00:52:08 -0500

    added a Task component to the Tasks. Shows the hard coded appointments. added a delete button from the installed npm react-icons. created the delete task function to delete the appointments when the 'x' is pressed.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
+import { useState } from 'react';
 import Header from './components/Header';
+import Tasks from './components/Tasks';
 
 function App(props) {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: "Doctor's Appointment",
+        day: "Feb 5th at 2:30pm",
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: "Meeting at School",
+        day: "Feb 6th at 1:30pm",
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: "Food Shopping",
+        day: "Feb 7th at 12:30pm",
+        reminder: false,
+    }
+])
+
+// Delete Task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+}
 
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,29 +1,13 @@
-const tasks = [
-    {
-        id: 1,
-        text: "Doctor's Appointment",
-        day: "Feb 5th at 2:30pm",
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: "Meeting at School",
-        day: "Feb 6th at 1:30pm",
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: "Food Shopping",
-        day: "Feb 7th at 12:30pm",
-        reminder: false,
-    }
-];
+import Task from './Task'
 
-const Tasks = () => {
+const Tasks = ({ tasks, onDelete }) => {
+    
     return (
-        <div>
-            
-        </div>
+        <>
+            {tasks.map((task) => (
+                <Task key={task.id} task={task} onDelete={onDelete} />
+            ))}
+        </>
     )
 }
 
