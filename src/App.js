a8f71883e3a81559fdc0fcbb120e5f1cafc87da1
Skyler R <skyler.rencher@gmail.com>
2021-03-03 13:07:16 -0500

    made changes to the add button to toggle the add task form when it is clicked. made small changes to the text of the button depending on which state it is in.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Header from './components/Header';
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
 
-function App(props) {
+const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -25,6 +26,14 @@ function App(props) {
     }
 ])
 
+// Add Task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000) + 1
+
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask])
+}
+
 // Delete Task
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
@@ -37,8 +46,8 @@ const toggleReminder = (id) => {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> ) : ( 'No Tasks To Show' ) }
     </div>
   );
